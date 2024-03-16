da39506144b18b002c298c083335e14fd0a46a4c
DevinFries <143852790+DevinFries@users.noreply.github.com>
2024-03-15 20:26:02 -0700

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 # Define database models
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    full_naqme = db.Column(db.String(100), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)  # Hashed password for security
